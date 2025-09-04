6ecae1d5774f06102d4aaf017105f4fc873cfc5b
praveen <116637585+praveen-047@users.noreply.github.com>
2025-09-04 11:12:47 +0530

    Initial commit: setup React project with routes

---
+++ src/components/Login/index.js
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+import './index.css'
+
+class Login extends Component{
+    render(){
+        return(
+            <div>
+                <h1>login</h1>
+            </div>
+        )
+    }
+}
+
+export default Login
---
+++ src/components/Signup/index.js
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+import './index.css'
+
+class Signup extends Component{
+    render(){
+        return(
+            <div>
+                <h1>signup</h1>
+            </div>
+        )
+    }
+}
+
+export default Signup
