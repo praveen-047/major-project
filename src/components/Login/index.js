3e4eaa583907e001cb071a81ee72d6481a8ab584
sinchana1404 <sinchanagowda1404@gmail.com>
2025-09-04 14:08:42 +0530

    Your message about what you changed

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,11 +1,23 @@
 import { Component } from 'react'
+
+import img1 from './images/img1.png'; 
+import img2 from './images/img2.png';
+import img3 from './images/img3.png';
 import './index.css'
+
 
 class Login extends Component{
     render(){
         return(
-            <div>
-                <h1>login</h1>
+            <div className='login-container'>
+                <div className='login-card'>
+                    <div className='login-avatar-container'>
+                        <img src = {img1} />
+                        <img src = {img2} />
+                        <img src = {img3} />
+                    </div>
+                </div>
+                
             </div>
         )
     }
