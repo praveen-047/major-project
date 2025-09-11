7ba68fd7791e15dcc5b5685f68dccbe53aae5959
sinchana1404 <sinchanagowda1404@gmail.com>
2025-09-11 12:19:15 +0530

    added meeting

---
+++ frontend/src/components/Meeting/index.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import "./index.css";
+class Meeting extends Component{
+    render(){
+        return(
+            <div className="meeting-container">
+                
+            </div>
+        )
+    }
+}
+
+export default Meeting;
