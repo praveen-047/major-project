c26ab7a8185347a3424be0dfd3242190eaccc663
PriyadarshiniK06 <150257110+PriyadarshiniK06@users.noreply.github.com>
2025-09-11 14:34:22 +0530

    Add Meeting folder inside frontend/src/components

---
+++ frontend/src/components/Meeting/index.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import './index.css'
+
+class Meeting extends Component{
+    render(){
+        return(
+            <div className="meeting-container"></div>
+        )
+    }
+}
+
+export default Meeting
